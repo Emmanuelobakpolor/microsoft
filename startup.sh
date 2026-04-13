3e43a06049b78be26b31bb164ee333e5976287e7
john <john@gmail.com>
2026-04-13 15:18:33 +0100

    Add startup script for Azure App Service

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Startup script for Azure App Service
+
+# Navigate to app directory
+cd /home/site/wwwroot
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run Django migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Start gunicorn
+gunicorn config.wsgi:application --bind=0.0.0.0:8000 --workers=4 --worker-class=sync
