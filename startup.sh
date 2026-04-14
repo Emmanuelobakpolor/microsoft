7368a7958d7baa9fbe92dd10d0f0d934bf7437cb
john <john@gmail.com>
2026-04-14 13:49:36 +0100

    DFD

--- startup.sh
+++ startup.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-# Startup script for Azure App Service
-
-# Run Django migrations
-python manage.py migrate
-
-# Collect static files
+python manage.py migrate --noinput
 python manage.py collectstatic --noinput
-
-# Start gunicorn
-gunicorn config.wsgi:application --bind=0.0.0.0:8000 --workers=4 --worker-class=sync
+gunicorn config.wsgi:application --bind=0.0.0.0:$PORT
